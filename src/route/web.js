48d450ea666353f605635297aa29761f5359b42d
Khanh Van <nvk26042003@gmail.com>
2025-06-12 10:07:09 +0700

    6th commit

--- src/route/web.js
+++ src/route/web.js
@@ -1,16 +1,17 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 let router = express.Router();
 let initWebRoutes = (app) => {
-    app.get('/', homeController.getHomePage)
-    app.get('/crud', homeController.getCRUD)
-    app.post('/post-crud', homeController.postCRUD)
-    app.get('/get-crud', homeController.displayGetCRUD)
-    app.get('/edit-crud', homeController.getEditCRUD)
-    app.post('/put-crud', homeController.putCRUD)
-    app.get('/delete-crud', homeController.deleteCRUD)
-    
+    router.get('/', homeController.getHomePage)
+    router.get('/crud', homeController.getCRUD)
+    router.post('/post-crud', homeController.postCRUD)
+    router.get('/get-crud', homeController.displayGetCRUD)
+    router.get('/edit-crud', homeController.getEditCRUD)
+    router.post('/put-crud', homeController.putCRUD)
+    router.get('/delete-crud', homeController.deleteCRUD)
+    router.post('/api/login', userController.handleLogin)
 
 
 
---
+++ src/services/userService.js
@@ -0,0 +1,69 @@
+import { where } from 'sequelize'
+import db from '../models/index'
+import bcrypt from 'bcryptjs'
+import { raw } from 'body-parser'
+// import { useState } from 'react'
+
+
+let handleUserLogin = (email, password) => {
+    return new Promise( async (resolve, reject) => {
+        try {
+            let userData = {};
+            let isExist = await checkUserEmail(email);
+
+            if(isExist){
+                //user is already exist    
+                let user =await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: {email: email},
+                    raw: true
+                })
+                //compare password
+                if(user){
+                    let check =await bcrypt.compareSync(password, user.password);
+                    if(check){
+                        userData.errCode = 0;
+                        userData.errMessage = 'OKE';
+                        delete user.password;
+                        userData.user = user
+                    }else{
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password'
+                    }
+                }else{
+                    userData.errCode = 2;
+                    userData.errMessage = `User's not found!`
+                }
+            }else{
+                //return error  
+                userData.errCode = 1;
+                userData.errMessage = `Your's email is not exist in your system. Please try orther email!`;
+            }
+            resolve(userData)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where:{email: userEmail}
+            })
+            if(user) {
+                resolve(true)
+            }else{
+                resolve(false)
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+module.exports = {
+    handleUserLogin: handleUserLogin,
+    
+}
